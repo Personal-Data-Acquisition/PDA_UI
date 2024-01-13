f95df0b6ae7f4b446260c84225df549e96344688
Blake <43260601+Leopard501@users.noreply.github.com>
2024-01-12 16:31:50 -0800

    Host wasm!

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,13 +1,26 @@
 #[macro_use] extern crate rocket;
+use rocket::fs::NamedFile;
 
 //The index
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn index() -> Result<NamedFile, std::io::Error> {
+    NamedFile::open("index.html").await
 }
 
+#[get("/pkg/hello_wasm.js")]
+async fn wasm() -> Result<NamedFile, std::io::Error> {
+    NamedFile::open("pkg/hello_wasm.js").await
+}
+
+#[get("/pkg/hello_wasm_bg.wasm")]
+async fn wasm_bg() -> Result<NamedFile, std::io::Error> {
+    NamedFile::open("pkg/hello_wasm_bg.wasm").await
+}
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![wasm])
+        .mount("/", routes![wasm_bg])
 }
