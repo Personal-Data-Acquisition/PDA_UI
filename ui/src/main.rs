0e869afed02809094336d183cfa4c637e49776ce
Blake <43260601+Leopard501@users.noreply.github.com>
2024-01-12 16:39:02 -0800

    Added comments

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -7,11 +7,15 @@ async fn index() -> Result<NamedFile, std::io::Error> {
     NamedFile::open("index.html").await
 }
 
+// get wasm entry js
+// this might be stupid... todo: revise later
 #[get("/pkg/hello_wasm.js")]
 async fn wasm() -> Result<NamedFile, std::io::Error> {
     NamedFile::open("pkg/hello_wasm.js").await
 }
 
+// get wasm file
+// this might be stupid... todo: revise later
 #[get("/pkg/hello_wasm_bg.wasm")]
 async fn wasm_bg() -> Result<NamedFile, std::io::Error> {
     NamedFile::open("pkg/hello_wasm_bg.wasm").await
