021c9313da68d0da7ed165860d530bcdaf627b9d
Blake <43260601+Leopard501@users.noreply.github.com>
2024-01-19 14:41:01 -0800

    egui rr progress

---
+++ ui/pkg/egui_wasm_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main(): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h75f41437eb890fc3(a: number, b: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hdf0ff132b1c73aeb(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h03bf33962ee03b8c(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hb3c3937d1080a6dc(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,30 +1,23 @@
 #[macro_use] extern crate rocket;
-use rocket::fs::NamedFile;
 
-//The index
+use rocket::fs::NamedFile;
+use rocket::http::{Status, ContentType};
+use std::path::{Path, PathBuf};
+
+// The index
 #[get("/")]
 async fn index() -> Result<NamedFile, std::io::Error> {
     NamedFile::open("index.html").await
 }
 
-// get wasm entry js
-// this might be stupid... todo: revise later
-#[get("/pkg/hello_wasm.js")]
-async fn wasm() -> Result<NamedFile, std::io::Error> {
-    NamedFile::open("pkg/hello_wasm.js").await
-}
-
-// get wasm file
-// this might be stupid... todo: revise later
-#[get("/pkg/hello_wasm_bg.wasm")]
-async fn wasm_bg() -> Result<NamedFile, std::io::Error> {
-    NamedFile::open("pkg/hello_wasm_bg.wasm").await
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("").join(file)).await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
-        .mount("/", routes![wasm])
-        .mount("/", routes![wasm_bg])
+        .mount("/", routes![files])
 }
