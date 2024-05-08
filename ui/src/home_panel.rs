ca4cf9017bb7854f2ed3e34a76a001069a5f1498
Blake <43260601+Leopard501@users.noreply.github.com>
2024-05-07 17:17:07 -0700

    Move map stuff into home panel ui fn

--- ui/src/home_panel.rs
+++ ui/src/home_panel.rs
@@ -3,9 +3,9 @@ use std::collections::HashMap;
 use egui::*;
 use egui_plot::{Legend, Line, Plot, PlotPoints};
 use wasm_bindgen_futures::spawn_local;
+use walkers::{Tiles, Map, MapMemory, Position, TilesManager, HttpOptions};
 use log::debug;
-use crate::utils::PollableValue;
-use crate::{Config, send_update};
+use crate::{send_update, Config, line_drawing::GpsLine, utils::PollableValue};
 
 // all of the auto-refreshing data on the home panel
 // displayed from first to last
@@ -16,10 +16,47 @@ const HOME_PANEL_KEYS: [&str; 3] = [
     "acceleration_z",
 ];
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Provider {
+    OpenStreetMap,
+    Geoportal,
+    MapboxStreets,
+    MapboxSatellite,
+    LocalTiles,
+}
+
+fn providers(egui_ctx: Context) -> HashMap<Provider, Box<dyn TilesManager + Send>> {
+    let mut providers: HashMap<Provider, Box<dyn TilesManager + Send>> = HashMap::default();
+
+    providers.insert(
+        Provider::OpenStreetMap,
+        Box::new(Tiles::with_options(
+            walkers::sources::OpenStreetMap,
+            http_options(),
+            egui_ctx.to_owned(),
+        )),
+    );  
+
+    providers
+}
+
+fn http_options() -> HttpOptions {
+    HttpOptions {
+        cache: if std::env::var("NO_HTTP_CACHE").is_ok() {
+            None
+        } else {
+            Some(".cache".into())
+        },
+    }
+}
+
 #[wasm_bindgen]
 pub struct HomePanel {
     is_recording: bool,
     data: HomePanelData,
+    map_memory: MapMemory,
+    providers: HashMap<Provider, Box<dyn TilesManager + Send>>,
+    gps_points: PollableValue<Vec<[f64; 2]>>,
 }
 
 /// refers to all of the auto-refreshing data on the home panel
@@ -47,8 +84,8 @@ impl HomePanelData {
     }
 }
 
-impl Default for HomePanel {
-    fn default() -> Self {
+impl HomePanel {
+    pub fn new(ctx: Context) -> Self {
         let mut defaults: HashMap<String, std::option::Option<std::vec::Vec<[f64; 2]>>> = HashMap::new();
         for key in HOME_PANEL_KEYS {
             defaults.insert(key.to_string(), None);
@@ -56,67 +93,94 @@ impl Default for HomePanel {
         Self {
             is_recording: false,
             data: HomePanelData::new(defaults),
+            map_memory: MapMemory::default(),
+            providers: providers(ctx),
+            gps_points: PollableValue::new(
+                Default::default(),
+                poll_promise::Promise::spawn_local(async {
+                    GpsLine::req_points().await
+                })
+            ),
         }
     }
-}
 
-impl HomePanel {
     pub fn ui(&mut self, ui: &mut Ui, config: &Config) {
-        let mut ready_count = 0;
-        // graphs showing auto-refreshing data
-        // keys should be provided in HOME_PANEL_KEYS
-        for key in HOME_PANEL_KEYS {
-            let val: &mut PollableValue<std::vec::Vec<[f64; 2]>> = self.data.data.get_mut(key).expect("missing key in HomePanelData");
-            if let Some(res) = val.poll() {
-                ready_count += 1;
-                let plot = Plot::new(key)
-                    .legend(Legend::default())
-                    .height(200.0);
-                let line = Line::new(PlotPoints::from(res)).name(key);
-                plot.show(ui, |plot_ui| {
-                    plot_ui.line(line);
-                });
-            }
-        }
-        // ui.heading(format!("ready count: {ready_count}"));
-        // ui.heading(format!("time: {t}", t=self.data.time));
-        // if all have been recieved, count up to refresh_time to refresh
-        if ready_count == HOME_PANEL_KEYS.len() {
-            self.data.time += 1;
-            if self.data.time == (config.refresh_time * 60.0) as u16 {
-                let mut defaults: HashMap<String, std::option::Option<Vec<[f64; 2]>>> = HashMap::new();
-                for key in HOME_PANEL_KEYS {
-                    let val: &mut PollableValue<Vec<[f64; 2]>> = self.data.data.get_mut(key)
-                        .expect("missing key in HomePanelData");
-                    defaults.insert(key.to_string(), val.poll());
+        ScrollArea::vertical()
+        .auto_shrink([false, false])
+        .stick_to_bottom(false)
+        .show(ui, |ui| {
+            let mut ready_count = 0;
+            // graphs showing auto-refreshing data
+            // keys should be provided in HOME_PANEL_KEYS
+            for key in HOME_PANEL_KEYS {
+                let val: &mut PollableValue<std::vec::Vec<[f64; 2]>> = self.data.data.get_mut(key).expect("missing key in HomePanelData");
+                if let Some(res) = val.poll() {
+                    ready_count += 1;
+                    let plot = Plot::new(key)
+                        .legend(Legend::default())
+                        .height(200.0);
+                    let line = Line::new(PlotPoints::from(res)).name(key);
+                    plot.show(ui, |plot_ui| {
+                        plot_ui.line(line);
+                    });
                 }
-                self.data = HomePanelData::new(
-                    defaults
-                )
             }
-        }
+            // ui.heading(format!("ready count: {ready_count}"));
+            // ui.heading(format!("time: {t}", t=self.data.time));
+            // if all have been recieved, count up to refresh_time to refresh
+            if ready_count == HOME_PANEL_KEYS.len() {
+                self.data.time += 1;
+                if self.data.time == (config.refresh_time * 60.0) as u16 {
+                    let mut defaults: HashMap<String, std::option::Option<Vec<[f64; 2]>>> = HashMap::new();
+                    for key in HOME_PANEL_KEYS {
+                        let val: &mut PollableValue<Vec<[f64; 2]>> = self.data.data.get_mut(key)
+                            .expect("missing key in HomePanelData");
+                        defaults.insert(key.to_string(), val.poll());
+                    }
+                    self.data = HomePanelData::new(
+                        defaults
+                    )
+                }
+            }
 
-        ui.horizontal(|ui| {
-            if !self.is_recording {
-                if ui.button("Record").clicked() {
-                    self.is_recording = true;
-                    let mut body = HashMap::new();
-                    body.insert("isRecording", "true");
-                    spawn_local(async move {
-                        send_update(&body, "http://127.0.0.1:8000/update").await;
-                    })
+            ui.horizontal(|ui| {
+                if !self.is_recording {
+                    if ui.button("Record").clicked() {
+                        self.is_recording = true;
+                        let mut body = HashMap::new();
+                        body.insert("isRecording", "true");
+                        spawn_local(async move {
+                            send_update(&body, "http://127.0.0.1:8000/update").await;
+                        })
+                    }
+                } else {
+                    if ui.button("Stop").clicked() {
+                        self.is_recording = false;
+                        let mut body = HashMap::new();
+                        body.insert("isRecording", "false");
+                        spawn_local(async move {
+                            send_update(&body, "http://127.0.0.1:8000/update").await;
+                        })
+                    }
                 }
-            } else {
-                if ui.button("Stop").clicked() {
-                    self.is_recording = false;
-                    let mut body = HashMap::new();
-                    body.insert("isRecording", "false");
-                    spawn_local(async move {
-                        send_update(&body, "http://127.0.0.1:8000/update").await;
-                    })
-                }
+            });
+
+            let tiles = self.providers.get_mut(&Provider::OpenStreetMap).unwrap().as_mut();
+
+            let mut map = Map::new(
+                Some(tiles),
+                &mut self.map_memory,
+                Position::from_lat_lon(44.56203897286608, -123.28196905234289));
+
+            if let Some(res) = self.gps_points.poll() {
+                map = map.with_plugin(GpsLine::new(res));
             }
+
+            ui.add_sized([ui.available_width(), 600.0], map);
+            zoom(ui, &mut self.map_memory);
         });
+
+        
     }
 
     /// Requests data of type `Option<Vec<[f64; 2]>>` from the server
@@ -141,4 +205,23 @@ impl HomePanel {
             }
         }
     }
+}
+
+pub fn zoom(ui: &Ui, map_memory: &mut MapMemory) {
+    Window::new("Map")
+        .collapsible(false)
+        .resizable(false)
+        .title_bar(false)
+        .anchor(Align2::LEFT_BOTTOM, [10., -10.])
+        .show(ui.ctx(), |ui| {
+            ui.horizontal(|ui| {
+                if ui.button(RichText::new("➕").heading()).clicked() {
+                    let _ = map_memory.zoom_in();
+                }
+
+                if ui.button(RichText::new("➖").heading()).clicked() {
+                    let _ = map_memory.zoom_out();
+                }
+            });
+        });
 }
--- ui/src/lib.rs
+++ ui/src/lib.rs
@@ -4,29 +4,14 @@ mod home_panel;
 mod log_panel;
 mod config_panel;
 
-use egui::*;
 use wasm_bindgen::prelude::*;
 use std::collections::HashMap;
 use log::debug;
 use wasm_bindgen_futures::spawn_local;
-use walkers::{Tiles, Map, MapMemory, Position, TilesManager, HttpOptions};
-use crate::{
-    line_drawing::GpsLine, 
-    utils::PollableValue, 
-    home_panel::*, 
-    log_panel::*, 
-    config_panel::*};
+use egui::Context;
+use crate::{home_panel::*, log_panel::*, config_panel::*};
 
 const TITLE: &str = "Personal Data Acquisition";
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum Provider {
-    OpenStreetMap,
-    Geoportal,
-    MapboxStreets,
-    MapboxSatellite,
-    LocalTiles,
-}
 
 #[wasm_bindgen]
 pub fn main() {
@@ -51,63 +36,17 @@ pub struct MyApp {
     home_panel: HomePanel,
     log_panel: LogPanel,
     config_panel: ConfigPanel,
-    map_memory: MapMemory,
-    providers: HashMap<Provider, Box<dyn TilesManager + Send>>,
-    gps_points: PollableValue<Vec<[f64; 2]>>,
-}
-
-impl Default for MyApp {
-    fn default() -> Self {
-        Self {
-            // tiles: Tiles::new(OpenStreetMap, Context::default()),
-            map_memory: MapMemory::default(),
-            open_panel: Panel::default(),
-            home_panel: HomePanel::default(),
-            log_panel: LogPanel::default(),
-            config_panel: ConfigPanel::default(),
-            providers: providers(Context::default()),
-            gps_points: PollableValue::new(
-                Default::default(),
-                poll_promise::Promise::spawn_local(async {
-                    GpsLine::req_points().await
-                })
-            ),
-        }
-    }
 }
 
 impl MyApp {
-    fn new(egui_ctx: Context) -> Self {
+    fn new(ctx: Context) -> Self {
         Self {
-            map_memory: MapMemory::default(),
             open_panel: Panel::default(),
-            home_panel: HomePanel::default(),
+            home_panel: HomePanel::new(ctx),
             log_panel: LogPanel::default(),
             config_panel: ConfigPanel::default(),
-            providers: providers(egui_ctx),
-            gps_points: PollableValue::new(
-                Default::default(),
-                poll_promise::Promise::spawn_local(async {
-                    GpsLine::req_points().await
-                }),
-            ),
         }
     }
-}
-
-fn providers(egui_ctx: Context) -> HashMap<Provider, Box<dyn TilesManager + Send>> {
-    let mut providers: HashMap<Provider, Box<dyn TilesManager + Send>> = HashMap::default();
-
-    providers.insert(
-        Provider::OpenStreetMap,
-        Box::new(Tiles::with_options(
-            walkers::sources::OpenStreetMap,
-            http_options(),
-            egui_ctx.to_owned(),
-        )),
-    );  
-
-    providers
 }
 
 impl eframe::App for MyApp {
@@ -118,31 +57,13 @@ impl eframe::App for MyApp {
                 ui.selectable_value(&mut self.open_panel, Panel::Log, "Log");
                 ui.selectable_value(&mut self.open_panel, Panel::Config, "Config");
             });
+
             ui.separator();
 
             match self.open_panel {
                 Panel::Home => {      
-                    ScrollArea::vertical()
-                        .auto_shrink([false, false])
-                        .stick_to_bottom(false)
-                        .show(ui, |ui| {
-                            self.home_panel.ui(ui, &self.config_panel.config);
-
-                            let tiles = self.providers.get_mut(&Provider::OpenStreetMap).unwrap().as_mut();
-
-                            let mut map = Map::new(
-                                Some(tiles),
-                                &mut self.map_memory,
-                                Position::from_lat_lon(44.56203897286608, -123.28196905234289));
-
-                            if let Some(res) = self.gps_points.poll() {
-                                map = map.with_plugin(GpsLine::new(res));
-                            }
-
-                            ui.add_sized([ui.available_width(), 600.0], map);
-                            zoom(ui, &mut self.map_memory);
-                        });
-                }
+                    self.home_panel.ui(ui, &self.config_panel.config);
+                },
                 Panel::Log => {
                     self.log_panel.ui(ui, &self.config_panel.config);
                 }
@@ -151,16 +72,6 @@ impl eframe::App for MyApp {
                 }
             }
         });
-    }
-}
-
-fn http_options() -> HttpOptions {
-    HttpOptions {
-        cache: if std::env::var("NO_HTTP_CACHE").is_ok() {
-            None
-        } else {
-            Some(".cache".into())
-        },
     }
 }
 
@@ -185,23 +96,4 @@ pub async fn send_update(body: &HashMap<&str, &str>, url: &str) {
             Err(e) => Err(e)
         };
     debug!("res: {:?}", res);
-}
-
-pub fn zoom(ui: &Ui, map_memory: &mut MapMemory) {
-    Window::new("Map")
-        .collapsible(false)
-        .resizable(false)
-        .title_bar(false)
-        .anchor(Align2::LEFT_BOTTOM, [10., -10.])
-        .show(ui.ctx(), |ui| {
-            ui.horizontal(|ui| {
-                if ui.button(RichText::new("➕").heading()).clicked() {
-                    let _ = map_memory.zoom_in();
-                }
-
-                if ui.button(RichText::new("➖").heading()).clicked() {
-                    let _ = map_memory.zoom_out();
-                }
-            });
-        });
 }
